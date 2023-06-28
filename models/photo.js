ed01afea00e1e9f1297759deb60896461a673177
andreshungbz <andres.hung@outlook.com>
2023-06-28 15:03:49 -0600

    feat: created Photo mongoose model

---
+++ models/photo.js
@@ -0,0 +1,35 @@
+// Photo Schema and Model for Uploaded Photos
+
+import { Schema, model, models } from 'mongoose';
+
+// create Photo schema
+const PhotoSchema = new Schema({
+  uploader: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  link: {
+    type: String,
+    required: [true, 'Link to an image is required.'],
+  },
+  title: {
+    type: String,
+    required: [true, 'Image title is required.'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Image description is required.'],
+  },
+  cameraModel: {
+    type: String,
+  },
+  uploadDate: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
+
+// check if model already exist, else create new Photo model
+const Photo = models.Photo || model('Photo', PhotoSchema);
+
+export default Photo;
