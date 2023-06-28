7494def1488525f6673a88ea08e070d740712c0a
andreshungbz <andres.hung@outlook.com>
2023-06-28 16:37:35 -0600

    feat: correctly retrieve photos in Gallery component

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,91 +1,48 @@
 // Photo Gallery Component
 
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 const Gallery = () => {
+  // state for populating photos
+  const [photos, setPhotos] = useState([]);
+
+  // effect for fetching all photos
+  useEffect(() => {
+    let ignore = false;
+    const fetchPhotos = async () => {
+      const response = await fetch('/api/photos');
+      const data = await response.json();
+      if (!ignore) {
+        setPhotos(data);
+      }
+    };
+    fetchPhotos();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <section className="gallery">
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
-      <div className="image-container">
-        <Image
-          src="/assets/images/placeholder.png"
-          alt="University of Belize Campus Photography Logo"
-          fill={true}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover"
-        />
-      </div>
+      {photos.map((photo) => {
+        return (
+          <div key={photo._id} className="image-container">
+            <Link href="/about">
+              <Image
+                src={photo.link}
+                alt={photo.title}
+                fill={true}
+                sizes="(max-width: 768px) 100vw, 45vw"
+                className="object-cover"
+              />
+            </Link>
+          </div>
+        );
+      })}
     </section>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // allow for Imgur images
+  // https://nextjs.org/docs/messages/next-image-unconfigured-host
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
