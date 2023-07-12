3618e35893aad1d6e73cd9efea90d4a3d091532e
andreshungbz <andres.hung@outlook.com>
2023-07-12 13:16:09 -0600

    refactor: changed photo page to a server component

--- app/photo/[id]/page.jsx
+++ app/photo/[id]/page.jsx
@@ -1,4 +1,11 @@
-import Photo from '@components/Photo';
+import Delete from '@components/Delete';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Photo from '@models/photo';
+import { connectMongoDB } from '@utils/database';
+
+import moment from 'moment';
 
 export const generateMetadata = async ({ params }) => {
   const id = params.id;
@@ -15,7 +22,50 @@ export const generateMetadata = async ({ params }) => {
   };
 };
 
-const PhotoDisplay = ({ params }) => {
-  return <Photo id={params.id} />;
+const PhotoDisplay = async ({ params }) => {
+  let photo;
+  try {
+    await connectMongoDB();
+    photo = await Photo.findById(params.id).populate('uploader');
+  } catch (error) {
+    console.log(error);
+    return <div className="text-center">Image Not Found</div>;
+  }
+
+  return (
+    photo && (
+      <section>
+        <div>
+          <Image
+            src={photo?.link}
+            alt={photo?.title}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="mx-auto h-auto w-min"
+          />
+          <div className="description-box">
+            <h1 className="title">{photo?.title}</h1>
+            <p className="text-sm">
+              <span className="bg-ub-purple-200 px-2 font-jetbrains">
+                Uploaded {moment(photo?.uploadDate).format('D MMMM YYYY')}
+              </span>
+            </p>
+            <Link
+              href={`/profile/${photo?.uploader?._id}`}
+              className="inline-link"
+            >
+              {photo?.uploader?.name}
+            </Link>
+            {photo?.cameraModel !== 'Unknown' && (
+              <p>{`Camera: ${photo?.cameraModel}`}</p>
+            )}
+            <p className="text-base">{photo?.description}</p>
+          </div>
+          <Delete photoData={JSON.stringify(photo)} />
+        </div>
+      </section>
+    )
+  );
 };
 export default PhotoDisplay;
---
+++ components/Delete.jsx
@@ -0,0 +1,50 @@
+// Photo Component for Displaying Single Photo and its Details
+
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+const Delete = ({ photoData }) => {
+  // retrieve user
+  const { data: session } = useSession();
+
+  // navigation router
+  const router = useRouter();
+
+  const photo = JSON.parse(photoData);
+
+  // function for deleting photo
+  const handleDelete = async (id) => {
+    const confirmation = confirm('Are you sure you want to delete this photo?');
+    if (confirmation) {
+      try {
+        await fetch(`/api/photo/${id}`, { method: 'DELETE' });
+        router.push('/');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      {session?.user.id &&
+        photo?.uploader?._id &&
+        session?.user?.id === photo?.uploader?._id && (
+          <div className="mt-2 text-center">
+            <button
+              type="button"
+              className="delete-btn"
+              onClick={() => {
+                handleDelete(photo._id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        )}
+    </div>
+  );
+};
+export default Delete;
